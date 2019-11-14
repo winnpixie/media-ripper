1cd328ff85f6dba38d11018b7e441a02c639520f
summer <salvadin2@gmail.com>
2019-11-13 22:24:04 -0500

    polish ups

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 (function () {
+    'use strict';
+
     class MediaExtractor {
         getInstagramMedia() {
             let postUrls = new Set();
@@ -35,12 +37,12 @@
         getInstagramStoryMedia() {
             let postUrls = new Set();
 
-            let videos = document.querySelectorAll("video");
+            let videos = document.querySelectorAll('video');
             if (videos.length > 0) {
                 // Don't get why they dont just use src, but whatever
                 postUrls.add(videos[0].currentSrc);
             } else {
-                postUrls.add(document.querySelectorAll("img")[1].src);
+                postUrls.add(document.querySelectorAll('img')[1].src);
             }
 
             return postUrls;
@@ -48,7 +50,7 @@
         getTwitterMedia() {
             let postUrls = new Set();
 
-            document.querySelectorAll("img[src*=\"format\"").forEach(elem => postUrls.add(elem.src.substring(0, elem.src.lastIndexOf("&name="))));
+            document.querySelectorAll('img[src*="format"').forEach(elem => postUrls.add(elem.src.substring(0, elem.src.lastIndexOf('&name='))));
 
             return postUrls;
         }
@@ -56,34 +58,34 @@
             let postUrls = new Set();
 
             // Grab videos instead of thumbnails if videos exist
-            let videoUrl = document.querySelectorAll("meta[property=\"og:video\"");
+            let videoUrl = document.querySelectorAll('meta[property="og:video"');
             if (videoUrl.length > 0) {
-                postUrls.add(document.querySelectorAll("meta[property=\"og:video\"")[0].content);
+                postUrls.add(document.querySelectorAll('meta[property="og:video"')[0].content);
             } else {
-                let imageUrl = document.querySelectorAll("meta[property=\"og:image\"")[0].content;
-                postUrls.add(imageUrl.substring(0, imageUrl.lastIndexOf("?h=")));
+                let imageUrl = document.querySelectorAll('meta[property="og:image"')[0].content;
+                postUrls.add(imageUrl.substring(0, imageUrl.lastIndexOf('?h=')));
             }
 
             return postUrls;
         }
         getMedia() {
-            if (window.location.host.includes("instagram.com")) {
-                if (window.location.pathname.startsWith("/stories")) {
+            if (window.location.host.includes('instagram.com')) {
+                if (window.location.pathname.startsWith('/stories/')) {
                     return this.getInstagramStoryMedia();
-                } else {
+                } else if (window.location.pathname.startsWith('/p/')) {
                     return this.getInstagramMedia();
                 }
-            } else if (window.location.host.includes("twitter.com")) {
+            } else if (window.location.host.includes('twitter.com')) {
                 return this.getTwitterMedia();
-            } else if (window.location.host.includes("vsco.co")) {
+            } else if (window.location.host.includes('vsco.co')) {
                 return this.getVscoMedia();
             }
             return [];
         }
     };
 
-    (new MediaExtractor()).getMedia().forEach(url => {
+    (window.MediaExtractor = new MediaExtractor()).getMedia().forEach(url => {
         console.log(url);
-        window.open(url, "_blank");
+        window.open(url, '_blank');
     });
 })();
