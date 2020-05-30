9d73431dfb8790bde36f1822351b196f40a08357
Summer <salvadin2@gmail.com>
2020-05-29 21:06:05 -0400

    clean up some code

--- index.js
+++ index.js
@@ -3,71 +3,76 @@
 
     class MediaExtractor {
         getInstagramMedia() {
-            // Data object is different depending on if you're logged in or not.
-            let data = null;
-            if (__additionalData[location.pathname]) {
-                data = __additionalData[location.pathname].data.graphql.shortcode_media;
-            } else {
-                data = _sharedData.entry_data.PostPage[0].graphql.shortcode_media;
-            }
+            let dataObject = __additionalData[location.pathname];
+            let post = dataObject != null ? dataObject.data.graphql.shortcode_media // Logged in
+                : _sharedData.entry_data.PostPage[0].graphql.shortcode_media; // Not logged in
 
-            if (data !== null) {
-                if (data.edge_sidecar_to_children) { // Multi-media post
-                    return data.edge_sidecar_to_children.edges
+            if (post != null) {
+                if (post.edge_sidecar_to_children != null) { // Multi-media post
+                    return post.edge_sidecar_to_children.edges
                         .map(edge => edge.node.is_video ? edge.node.video_url : edge.node.display_url);
-                } else if (data.is_video) { // Single-video post
-                    return [data.video_url];
                 }
-
-                // Single-image post
-                return [data.display_url];
+                return post.is_video ? [post.video_url] : [post.display_url];
             }
 
+            console.log("Are you sure you're on an Instagram post?");
             return [];
         }
-        getInstagramStoryMedia() {
+        getInstagramStoryMedia() { // NOTE: Stories require you to be logged in.
             let videos = document.getElementsByTagName('video');
             if (videos.length > 0) {
                 return [videos[0].currentSrc];
             }
 
-            // When you're on mobile the index should be 0, weird.
-            return [document.getElementsByTagName('img')[navigator.userAgent.includes("Mobile") ? 0 : 1].src];
+            // When on mobile the image index is 0 instead of 1 like on desktop, weird.
+            return [document.getElementsByTagName('img')[navigator.userAgent.includes('Mobile') ? 0 : 1].src];
         }
-        getTikTokMedia() {
+        getTikTokMedia() { // NOTE: This returns a .htm file on mobile, external programs are needed for renaming.
             return [document.getElementsByTagName('video')[0].src];
         }
-        getTwitterMedia() {
+        getTwitterMedia() { // NOTE: Videos require sniffing out XMLHttpRequest connections and external programs, undesirable solution.
+            // NOTE: This will also return some images within replies.
             return Array.from(document.querySelectorAll('img[src*="format"]'))
                 .map(elem => elem.src.substring(0, elem.src.lastIndexOf('&')));
         }
-        getVscoMedia() {
-            let videos = document.querySelectorAll('meta[property="og:video"]');
-            if (videos.length > 0) {
-                return [videos[0].content];
+        getVscoMedia() { // ez-pz reading <meta> tags.
+            let video = document.querySelector('meta[property="og:video"]');
+            if (video != null) {
+                return [video.content];
             }
 
-            let image = document.querySelectorAll('meta[property="og:image"]')[0].content;
+            let image = document.querySelector('meta[property="og:image"]').content;
             return [image.substring(0, image.lastIndexOf('?'))];
         }
         getMedia() {
             let host = location.host;
             let path = location.pathname;
 
+            // Instagram
             if (host.includes('instagram.com')) {
-                if (path.startsWith('/stories/')) {
-                    return this.getInstagramStoryMedia();
-                } else if (path.startsWith('/p/') || path.startsWith('/tv/')) {
+                // Posts/IG-TV
+                if (path.startsWith('/p/') || path.startsWith('/tv/')) {
                     return this.getInstagramMedia();
                 }
-            } else if (host.includes('tiktok.com')) {
+                // Stories
+                if (path.startsWith('/stories/')) {
+                    return this.getInstagramStoryMedia();
+                }
+            }
+            // TikTok
+            if (host.includes('tiktok.com')) {
                 return this.getTikTokMedia();
-            } else if (host.includes('twitter.com')) {
+            }
+            // Twitter
+            if (host.includes('twitter.com')) {
                 return this.getTwitterMedia();
-            } else if (host.includes('vsco.co')) {
+            }
+            // Vsco
+            if (host.includes('vsco.co')) {
                 return this.getVscoMedia();
             }
 
+            console.log(`${host} is not supported.`);
             return [];
         }
     };
