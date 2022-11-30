de6122b6f90e7ab80038165ae711dc393d7198a4
Hannah <salvadin2@gmail.com>
2022-11-30 03:06:38 -0500

    update version...?

--- socialrip.user.js
+++ socialrip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         socialrip
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Extract media from social platforms so you can save them locally.
 // @author       Hannah
 // @match        https://*.instagram.com/*
